8439324af744e440c01f969c06b4bc4c323d2159
Praveen Dias <praveendias1180@gmail.com>
2022-12-30 16:10:07 +0530

    Initial commit 🚀

---
+++ README.md
@@ -0,0 +1,3 @@
+Create a Block Tutorial
+
+https://developer.wordpress.org/block-editor/getting-started/create-block/
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'bc28cf32e2e4a18c935d');
